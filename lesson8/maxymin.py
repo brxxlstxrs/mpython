281acc9ffdaaf20f1288650b042cbf747a75d52a
cuc(phone) <brxxlstxrs@gmail.com>
2021-11-02 19:12:19 +0300

    go to pc

--- lesson8/maxymin.py
+++ lesson8/maxymin.py
@@ -1,39 +1,26 @@
-
-b1 = 0
-firsttime1 = 1
-firsttime2 = 1
-bglobal = 0
-c = 0
+count = 0
+flag1 = 1
+flag2 = 1
 a = 0
 b = 0
+bglobal = 0
+n1 = 0
 n = int(input())
 for i in range(n):
-    c = int(input())
-    for j in range(c):
-        if firsttime1:
-            b = int(input())
-            firsttime1 = 0
+    n1 = int(input())
+    for j in range(n1):
         a = int(input())
-        if a > b:
-            b = b
-        else:
-            b = a            
-    firsttime1 = 1
-    if firsttime2:
-        c = int(input())
-        for j in range(c):
-            if firsttime1:
-                b1 = int(input())
-                firsttime1 = 0
-            a = int(input())
-            if a > b1:
-                b = b1
-            else:
-                b1 = a
-        bglobal = b1
-        firsttime2 = 0
-    if bglobal > b:
+        if flag1:
+            b = a
+            flag1 = 0
+        if a < b:
+            b = a
+    flag1 = 1
+    if flag2:
+        bglobal = b
+        flag2 = 0
+    if bglobal < b:
         bglobal = b
     else:
-        bglobal = bglobal
-print(bglobal)
+        count = i + 1
+print(count, bglobal)
