0cdac79d2e6b73b3d477b4787ff4ea77e2441477
cuc <brxxlstxrs@gmail.com>
2021-11-14 02:34:15 +0300

    sets hw

---
+++ lesson9/howoften.py
@@ -0,0 +1,12 @@
+last = set()
+now = set()
+n = int(input())
+for i in range(n):
+    m = int(input())
+    for j in range(m):
+        now.add(input())
+    if i == 0:
+        last = now
+    last = last & now
+print(*last, sep='\n')
+
